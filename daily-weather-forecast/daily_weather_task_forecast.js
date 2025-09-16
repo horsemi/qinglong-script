60b22e13d28a4a38727d7d81ef4d8db8a2348836
horsemi <horsemi@outlook.com>
2025-09-16 16:18:31 +0800

    fix: 增加预警

--- daily-weather-forecast/daily_weather_task_forecast.js
+++ daily-weather-forecast/daily_weather_task_forecast.js
@@ -9,7 +9,7 @@ let ACC_WEATHER_API_KEY = '';
 const ACC_WEATHER_URL = 'https://nq73jprfpp.re.qweatherapi.com';
 const ACC_WEATHER_LANGUAGE = 'zh-hans';
 const ACC_WEATHER_LOCATION_KEY = process.env.ACC_WEATHER_LOCATION_KEY; //  地区ID
-const ACC_WEATHER_INDICES_KEY = '0'; //  穿衣指数
+const ACC_WEATHER_INDICES_KEY = '0'; //  全部指数
 
 class WeatherTaskBot {
   async run() {
@@ -22,11 +22,15 @@ class WeatherTaskBot {
 
     const { indicesDetail } = await this.getIndices();
 
-    const { dailyForecastsDetail } = await this.getForecasts();
+    const { dailyForecastsDetail, fxLink } = await this.getForecasts();
+
+    const { warning } = await this.getWarning();
 
     return {
       indicesDetail,
       dailyForecastsDetail,
+      fxLink,
+      warning,
     };
   }
 
@@ -61,7 +65,7 @@ class WeatherTaskBot {
         indicesDetail: _responseData,
       };
     } else {
-      throw new Error('请求失败, 请检查是否调用次数');
+      throw new Error('请求失败' + response);
     }
   }
 
@@ -93,9 +97,44 @@ class WeatherTaskBot {
 
       return {
         dailyForecastsDetail: _responseData[1],
+        fxLink: data.fxLink,
       };
     } else {
-      throw new Error('请求失败, 请检查是否调用次数');
+      throw new Error('请求失败' + response);
+    }
+  }
+
+  async getWarning() {
+    if (!ACC_WEATHER_LOCATION_KEY) {
+      $.logErr(
+        'ACC_WEATHER_LOCATION_KEY变量(地区ID)为空，请配置后再使用 url:https://developer.accuweather.com/'
+      );
+      throw new Error('LOCATION_KEY变量(地区ID)为空');
+    }
+
+    const { isSuccess, data, response } = await requestApi(
+      `${ACC_WEATHER_URL}/v7/warning/now`,
+      {
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${ACC_WEATHER_API_KEY}`,
+        },
+        data: {
+          location: ACC_WEATHER_LOCATION_KEY,
+          lang: ACC_WEATHER_LANGUAGE,
+        },
+      }
+    );
+
+    if (isSuccess) {
+      const _responseData = data.warning;
+      $.log(`请求成功，信息为: ${JSON.stringify(_responseData)}`);
+
+      return {
+        warning: _responseData,
+      };
+    } else {
+      throw new Error('请求失败' + response);
     }
   }
 }
@@ -103,23 +142,29 @@ class WeatherTaskBot {
 !(async () => {
   const bot = new WeatherTaskBot();
 
-  const { indicesDetail, dailyForecastsDetail } = await bot.run();
+  const { indicesDetail, dailyForecastsDetail, fxLink, warning } =
+    await bot.run();
+  const warningMsg = Array.isArray(warning)
+    ? warning.map((item) => item.title).join('\n') || '无'
+    : '无';
 
-  const msg = `蝶宝，现在是${new Date().toLocaleString()}，\n明天最高温度${
+  const msg = `蝶宝，现在是${new Date().toLocaleString()}，\n明天白天${
+    dailyForecastsDetail.textDay
+  }，夜晚${dailyForecastsDetail.textNight}，最高温度${
     dailyForecastsDetail.tempMax
-  }℃，最低温度${dailyForecastsDetail.tempMin}℃，相对湿度为${
-    dailyForecastsDetail.humidity
-  }，日出时间为${dailyForecastsDetail.sunrise}，\n明日防晒指数：${
+  }℃，最低温度${dailyForecastsDetail.tempMin}℃，\n明日防晒指数：${
     indicesDetail.find((item) => item.type === '16').text
-  }\n明日晾晒指数：${indicesDetail.find((item) => item.type === '14').text}`;
-
+  }\n明日晾晒指数：${
+    indicesDetail.find((item) => item.type === '14').text
+  }\n天气预警：${warningMsg}`;
+  console.log('msg: ', msg);
   await notify.sendNotify(
     `小鹿贴心穿衣指数预报: ${
       indicesDetail.find((item) => item.type === '3').text
     }`,
     msg,
     {
-      // url: indicesDetail.MobileLink,
+      url: fxLink,
       // group: "xiaolu_trips",
       // sound: 'minuet.typewriters'
     },
